458d942a1f3cf4025739756ca7a0bfdedb4801f9
Rifat Muhtasim <67732890+RifatMuhtasim@users.noreply.github.com>
2023-09-25 21:40:28 +0600

    Update settings.py

--- ZaraCorporation/lab/settings.py
+++ ZaraCorporation/lab/settings.py
@@ -149,8 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tablab.bd@gmail.com'
-EMAIL_HOST_PASSWORD = 'AlphaBetaGamma'
+
 
 # EMAIL_HOST_USER = 'KhandokarRifatWasi'
 # EMAIL_HOST_PASSWORD = '1234'
